dbe4e4a72c9bcd7118f139c07cf9682661fa5742
Zeynep Kizilkaya <zeynepyildiz.kizilkaya@gmail.com>
2026-07-08 14:30:42 +0300

    Initial Spring Boot project setup with Oracle Docker

---
+++ src/main/java/com/zeynep/rdbmswithoracle/RdbmsWithOracleApplication.java
@@ -0,0 +1,13 @@
+package com.zeynep.rdbmswithoracle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RdbmsWithOracleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RdbmsWithOracleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zeynep/rdbmswithoracle/RdbmsWithOracleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zeynep.rdbmswithoracle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RdbmsWithOracleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
